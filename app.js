2645b1537e5dd81cc1a208c243f3b2c8da77ef0b
dan850 <dan06982@gmail.com>
2022-08-31 03:11:11 -0500

    change

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 function myFunction() {
    
    console.log("What is your favorite programming language?");
-     document.write("What is your favorite programming language?");
+    // document.write("What is your favorite programming language?");
+
+    let x, y;
+    x = 5;
+    y = 6;
+
+    console.log(x+y);
+
+
+     document.getElementById("demo").innerHTML = "What is your favorite programming language?";
+
+let cookies = 0;
+let 
+
+
 }
+
